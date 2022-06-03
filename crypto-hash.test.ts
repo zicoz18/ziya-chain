d326ab8c2bad11bcbabe6c355d85a90db2ef489b
zicoz18 <zicoz18@ku.edu.tr>
2022-06-04 01:32:56 +0300

    Add crpyto-hash tests

---
+++ crypto-hash.test.ts
@@ -0,0 +1,17 @@
+import cryptoHash from "./crypto-hash";
+
+describe("cryptoHash()", () => {
+	// 2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae
+
+	it("generates a SHA-256 hashed output", () => {
+		expect(cryptoHash("foo")).toEqual(
+			"2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae"
+		);
+	});
+
+	it("produces the same hash with the same input arguments in any order", () => {
+		expect(cryptoHash("one", "two", "three")).toEqual(
+			cryptoHash("three", "one", "two")
+		);
+	});
+});

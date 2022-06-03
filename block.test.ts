ad98f8fdba086bdb7e5de568489bb110313d49cb
zicoz18 <zicoz18@ku.edu.tr>
2022-06-04 00:51:15 +0300

    Add Block tests

---
+++ block.test.ts
@@ -0,0 +1,16 @@
+import Block from "./block";
+
+describe("Block", () => {
+	const timestamp = "a-date";
+	const lastHash = "foo-hash";
+	const hash = "bar-hash";
+	const data = ["blockchain", "data"];
+	const block = new Block({ timestamp, lastHash, hash, data });
+
+	it("has a timestamp, lastHash, hash, data property", () => {
+		expect(block.timestamp).toEqual(timestamp);
+		expect(block.lastHash).toEqual(lastHash);
+		expect(block.hash).toEqual(hash);
+		expect(block.data).toEqual(data);
+	});
+});

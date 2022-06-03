813ebd9c53d2e409e9597f9dd91eb8e9027acf7d
zicoz18 <zicoz18@ku.edu.tr>
2022-06-04 01:58:30 +0300

    Add Blockchain tests

---
+++ tests/blockchain.test.ts
@@ -0,0 +1,21 @@
+import Blockchain from "../models/blockchain";
+import Block from "../models/block";
+
+describe("Blockchain", () => {
+	const blockchain = new Blockchain();
+
+	it("contains a `chain` Array instance", () => {
+		expect(blockchain.chain instanceof Array).toBe(true);
+	});
+
+	it("starts with a genesis block", () => {
+		expect(blockchain.chain[0]).toEqual(Block.genesis());
+	});
+
+	it("adds a new block to the chain", () => {
+		const newData = "foo bar";
+		blockchain.addBlock({ data: newData });
+
+		expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(newData);
+	});
+});

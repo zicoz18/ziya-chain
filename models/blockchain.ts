86f23d17e64c58886c243326ab047abf4e0df25b
zicoz18 <zicoz18@ku.edu.tr>
2022-06-04 09:10:40 +0300

    Implement isValidChain

--- models/blockchain.ts
+++ models/blockchain.ts
@@ -1,4 +1,5 @@
 import Block from "./block";
+import cryptoHash from "../utils/crypto-hash";
 
 class Blockchain {
 	public chain: any[];
@@ -14,6 +15,24 @@ class Blockchain {
 		});
 
 		this.chain.push(newBlock);
+	}
+
+	public static isValidChain(chain: any[]) {
+		if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis()))
+			return false;
+
+		for (let i = 1; i < chain.length; i++) {
+			const { timestamp, lastHash, data, hash } = chain[i];
+			const actualLastHash = chain[i - 1].hash;
+
+			if (actualLastHash !== lastHash) return false;
+
+			const validatedHash = cryptoHash(timestamp, lastHash, data);
+
+			if (validatedHash !== hash) return false;
+		}
+
+		return true;
 	}
 }
 

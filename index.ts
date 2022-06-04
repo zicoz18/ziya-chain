c7cd3aebac4be71188d2f1a6959ef0f9b435fd23
zicoz18 <zicoz18@ku.edu.tr>
2022-06-04 20:31:53 +0300

    Sync newly added peers

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import axios from "axios";
 import Blockchain from "./models/blockchain";
 import PubSub from "./models/pubsub";
 
@@ -8,6 +9,9 @@ const main = async () => {
 	app.use(bodyParser.json());
 	const blockchain = new Blockchain();
 	const pubsub = await PubSub.create({ blockchain });
+
+	const DEFAULT_PORT = 3000;
+	const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}/api/blocks`;
 
 	pubsub.broadcastChain();
 
@@ -19,11 +23,33 @@ const main = async () => {
 		const { data } = req.body;
 
 		blockchain.addBlock({ data });
+		pubsub.broadcastChain();
 		res.redirect("api/blocks");
 	});
 
-	const PORT = 3000;
+	const syncChains = async () => {
+		try {
+			const response = await axios.get(ROOT_NODE_ADDRESS);
+			if (response.status === 200) {
+				console.log("replace chain on a sync with: ", response.data);
+				blockchain.replaceChain(response.data);
+			} else {
+				console.log("not inside");
+			}
+		} catch (err) {
+			console.error("Error syncing the chain: ", err);
+		}
+	};
+
+	let PEER_PORT;
+
+	if (process.argv[2] === "true") {
+		PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+	}
+
+	const PORT = PEER_PORT || DEFAULT_PORT;
 	app.listen(PORT, () => console.log(`Listening at localhost:${PORT}`));
+	await syncChains();
 };
 
 main();

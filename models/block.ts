c526c7fe8183fb9bcdffd80102084c1050b2dad7
zicoz18 <zicoz18@ku.edu.tr>
2022-06-04 11:44:55 +0300

    Update using hexToBinary()

--- models/block.ts
+++ models/block.ts
@@ -1,5 +1,6 @@
 import { GENESIS_DATA, MINE_RATE } from "../config";
 import cryptoHash from "../utils/crypto-hash";
+import hexToBinary from "../utils/hex-to-binary";
 
 class Block {
 	public timestamp: any;
@@ -36,7 +37,9 @@ class Block {
 				timestamp,
 			});
 			hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
-		} while (hash.substring(0, difficulty) !== "0".repeat(difficulty));
+		} while (
+			hexToBinary(hash).substring(0, difficulty) !== "0".repeat(difficulty)
+		);
 
 		return new Block({ timestamp, lastHash, hash, difficulty, nonce, data });
 	}
--- tests/block.test.ts
+++ tests/block.test.ts
@@ -1,6 +1,7 @@
 import Block from "../models/block";
 import { GENESIS_DATA, MINE_RATE } from "../config";
 import cryptoHash from "../utils/crypto-hash";
+import hexToBinary from "../utils/hex-to-binary";
 
 describe("Block", () => {
 	const timestamp = 2000;
@@ -73,9 +74,9 @@ describe("Block", () => {
 		});
 
 		it("sets a `hash` that matches the difficulty criteria", () => {
-			expect(minedBlock.hash.substring(0, minedBlock.difficulty)).toEqual(
-				"0".repeat(minedBlock.difficulty)
-			);
+			expect(
+				hexToBinary(minedBlock.hash).substring(0, minedBlock.difficulty)
+			).toEqual("0".repeat(minedBlock.difficulty));
 		});
 
 		it("adjusts the difficulty", () => {

b36b0a49099fefbc889abe98ef5bed268b85c353
zicoz18 <zicoz18@ku.edu.tr>
2022-06-04 10:40:53 +0300

    Update mineBlock()

--- models/block.ts
+++ models/block.ts
@@ -6,12 +6,16 @@ class Block {
 	public lastHash: any;
 	public hash: any;
 	public data: any;
+	public nonce: any;
+	public difficulty: any;
 
-	constructor({ timestamp, lastHash, hash, data }: any) {
+	constructor({ timestamp, lastHash, hash, data, nonce, difficulty }: any) {
 		this.timestamp = timestamp;
 		this.lastHash = lastHash;
 		this.hash = hash;
 		this.data = data;
+		this.nonce = nonce;
+		this.difficulty = difficulty;
 	}
 
 	public static genesis(): any {
@@ -19,11 +23,19 @@ class Block {
 	}
 
 	public static mineBlock({ lastBlock, data }: any): any {
-		const timestamp = Date.now();
-		const lastHash = lastBlock.hash;
-		const hash = cryptoHash(timestamp, lastHash, data);
+		let hash, timestamp;
+		let nonce = 0;
 
-		return new Block({ timestamp, lastHash, hash, data });
+		const lastHash = lastBlock.hash;
+		const { difficulty } = lastBlock;
+
+		do {
+			nonce++;
+			timestamp = Date.now();
+			hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
+		} while (hash.substring(0, difficulty) !== "0".repeat(difficulty));
+
+		return new Block({ timestamp, lastHash, hash, difficulty, nonce, data });
 	}
 }
 

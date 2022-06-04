32851cb74ae7748d622d674bcf3a16d5643aff5a
zicoz18 <zicoz18@ku.edu.tr>
2022-06-04 18:12:03 +0300

    Implement basic API

--- index.ts
+++ index.ts
@@ -1,1 +1,21 @@
-console.log("works fine");
+import express from "express";
+import bodyParser from "body-parser";
+import Blockchain from "./models/blockchain";
+
+const app = express();
+app.use(bodyParser.json());
+const blockchain = new Blockchain();
+
+app.get("/api/blocks", (req, res) => {
+	res.json(blockchain.chain);
+});
+
+app.post("/api/mine", (req, res) => {
+	const { data } = req.body;
+
+	blockchain.addBlock({ data });
+	res.redirect("api/blocks");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Listening at localhost:${PORT}`));

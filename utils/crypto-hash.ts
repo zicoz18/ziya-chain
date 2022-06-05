b62885fe4f623ab67fb1b1291dc2fcec55ff884d
zicoz18 <zicoz18@ku.edu.tr>
2022-06-05 21:00:22 +0300

    Improve cryptoHash() to change the hash when object values change

--- utils/crypto-hash.ts
+++ utils/crypto-hash.ts
@@ -2,8 +2,15 @@ import crypto from "crypto";
 
 const cryptoHash = (...inputs: any[]) => {
 	const hash = crypto.createHash("sha256");
-	const hashOutput = hash.update(inputs.sort().join(" ")).digest("hex");
-	return hashOutput;
+
+	hash.update(
+		inputs
+			.map((input) => JSON.stringify(input))
+			.sort()
+			.join(" ")
+	);
+
+	return hash.digest("hex");
 };
 
 export default cryptoHash;

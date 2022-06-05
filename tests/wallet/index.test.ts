9003ecbfeab5e4b94212ab51714fa095f1903d71
zicoz18 <zicoz18@ku.edu.tr>
2022-06-05 20:28:52 +0300

    Test createTransaction()

--- tests/wallet/index.test.ts
+++ tests/wallet/index.test.ts
@@ -1,5 +1,6 @@
 import Wallet from "../../wallet";
 import { verifySignature } from "../../utils";
+import Transaction from "../../wallet/transaction";
 
 describe("Wallet", () => {
 	let wallet: Wallet;
@@ -38,6 +39,40 @@ describe("Wallet", () => {
 					signature: new Wallet().sign(data),
 				})
 			).toBe(false);
+		});
+	});
+
+	describe("createTransaction()", () => {
+		describe("and the amount exceeds the balance", () => {
+			it("throws an error", () => {
+				expect(() =>
+					wallet.createTransaction({
+						amount: 999999,
+						recipient: "foo-recipient",
+					})
+				).toThrow("Amount exceeds balance");
+			});
+		});
+
+		describe("and the amount is valid", () => {
+			let transaction: Transaction, amount: number, recipient: string;
+
+			beforeEach(() => {
+				amount: 50;
+				recipient: "foo-recipient";
+				transaction = wallet.createTransaction({ amount, recipient });
+			});
+			it("creates an instance of `Transaction`", () => {
+				expect(transaction instanceof Transaction).toBe(true);
+			});
+
+			it("matches the transaction input with the wallet", () => {
+				expect(transaction.input.address).toEqual(wallet.publicKey);
+			});
+
+			it("outputs the amount the recipient", () => {
+				expect(transaction.outputMap[recipient]).toEqual(amount);
+			});
 		});
 	});
 });

ff4bf5199b0190b1bdedf6db69e069d3474b8446
zicoz18 <zicoz18@ku.edu.tr>
2022-06-05 23:19:16 +0300

    Improve types for verifySignature()

--- utils/index.ts
+++ utils/index.ts
@@ -1,10 +1,20 @@
-import { ec as ES } from "elliptic";
+import { ec as EC } from "elliptic";
 import cryptoHash from "./crypto-hash";
 
-const ec = new ES("secp256k1");
+interface VerifySignatureAttributes {
+	publicKey: string;
+	data: any;
+	signature: EC.Signature;
+}
 
-const verifySignature = ({ publicKey, data, signature }: any) => {
-	const keyFromPublic = ec.keyFromPublic(publicKey, "hex");
+const ec = new EC("secp256k1");
+
+const verifySignature = ({
+	publicKey,
+	data,
+	signature,
+}: VerifySignatureAttributes): boolean => {
+	const keyFromPublic: EC.KeyPair = ec.keyFromPublic(publicKey, "hex");
 
 	return keyFromPublic.verify(cryptoHash(data), signature);
 };

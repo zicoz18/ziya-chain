87280dfd1a2ed11142e6e625deb0fd5f700d2fac
zicoz18 <zicoz18@ku.edu.tr>
2022-06-09 11:51:34 +0300

    Update handleMessage() for BLOCKCHAIN channel so that peers clear their transactionPool

--- app/pubsub.ts
+++ app/pubsub.ts
@@ -59,7 +59,11 @@ class PubSub {
 
 		switch (channel) {
 			case CHANNELS.BLOCKCHAIN:
-				this.blockchain.replaceChain(parsedMessage);
+				this.blockchain.replaceChain(parsedMessage, () => {
+					this.transactionPool.clearBlockchainTransactions({
+						chain: parsedMessage,
+					});
+				});
 				break;
 			case CHANNELS.TRANSACTION:
 				const newlyCreatedTransaction = new Transaction({

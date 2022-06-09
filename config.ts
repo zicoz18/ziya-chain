8068b12881a94164d4ab2b6bfa083f06c5144039
zicoz18 <zicoz18@ku.edu.tr>
2022-06-09 11:48:53 +0300

    Add REWARD_INPUT to config

--- config.ts
+++ config.ts
@@ -12,4 +12,30 @@ const GENESIS_DATA = {
 
 const STARTING_BALANCE = 1000;
 
-export { GENESIS_DATA, MINE_RATE, STARTING_BALANCE };
+const REWARD_INPUT = <
+	{
+		timestamp: number;
+		amount: number;
+		address: string;
+		signature: any;
+	}
+>{
+	timestamp: 0,
+	amount: 0,
+	address: "*authorized-reward*",
+	signature: {
+		r: "0",
+		s: "0",
+		recoveryParam: 0,
+	},
+};
+
+const MINING_REWARD = 50;
+
+export {
+	GENESIS_DATA,
+	MINE_RATE,
+	STARTING_BALANCE,
+	REWARD_INPUT,
+	MINING_REWARD,
+};
